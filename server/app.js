8135cc16c30a563c8199c53d52bcd55f46d9e605
austin samsel <austinsamsel@gmail.com>
2015-09-30 23:25:22 -0400

    rm dupe publish

--- server/app.js
+++ server/app.js
@@ -1,7 +1,3 @@
-Meteor.publish('posts', function(){
-  return Posts.find();
-});
-
 // On server startup, create some posts if DB is empty
 if (Meteor.isServer) {
   Meteor.publish("posts", function(){
