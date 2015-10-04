04b03fcbacbfed6cc90bd25440ab03e1dace71a9
austin samsel <austinsamsel@gmail.com>
2015-10-04 00:08:41 -0400

    add flow router, links to individual posts

---
+++ client/app.js
@@ -0,0 +1,37 @@
+if (Meteor.isClient) {
+
+  Meteor.subscribe('posts');
+
+
+  Template.homePage.helpers({
+    posts: function () {
+      return Posts.find({});
+    }
+  });
+  Template.singlePost.onCreated(function() {
+    var self = this;
+    self.autorun(function() {
+      var postId = FlowRouter.getParam('postId');
+      self.subscribe('post', postId);
+    });
+  });
+  Template.singlePost.helpers({
+    post: function() {
+      var postId = FlowRouter.getParam('postId');
+      var post = Posts.findOne({_id: postId}) || {};
+      return post;
+    }
+  });
+
+  //
+  Template.post.helpers({
+    pathForPost: function() {
+      var post = this;
+      var postId = post._id;
+      var path = FlowRouter.path(postId);
+      return path;
+    }
+  });
+
+
+}
