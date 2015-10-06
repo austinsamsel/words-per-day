e1e543c4b55ae4e13907b4dbf7aadd94364c813b
austin samsel <austinsamsel@gmail.com>
2015-10-05 20:39:09 -0400

    routes working

--- model/posts.js
+++ model/posts.js
@@ -1,22 +1,11 @@
 Posts = new Mongo.Collection('posts');
 
-// if (Meteor.isClient) {
-//   // This code only runs on the client
-//   Template.body.helpers({
-//     tasks: [
-//       { text: "This is task 1" },
-//       { text: "This is task 2" },
-//       { text: "This is task 3" }
-//     ]
-//   });
-// }
-
 if (Meteor.isClient) {
 
   Meteor.subscribe('posts');
 
   // This code only runs on the client
-  Template.body.helpers({
+  Template.layout.helpers({
     posts: function () {
       return Posts.find({});
     }
