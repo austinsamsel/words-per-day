6f14348c98e41f7c7d05ed2e7c4f2760dfcd084c
austin samsel <austinsamsel@gmail.com>
2015-10-06 22:55:29 -0400

    update fixtures and add in event for create post form

--- model/posts.js
+++ model/posts.js
@@ -2,7 +2,7 @@ Posts = new Mongo.Collection('posts');
 
 if (Meteor.isClient) {
 
-  Meteor.subscribe('posts');
+  // Meteor.subscribe('posts');
 
   // This code only runs on the client
   Template.body.helpers({
@@ -10,8 +10,23 @@ if (Meteor.isClient) {
       return Posts.find({});
     }
   });
-
   Template.registerHelper('cleanDate', function(date) {
   	return moment(date).format('MM-DD-YYYY');
   });
+
+  Template.createPost.events({
+    'submit form': function(e){
+      e.preventDefault();
+      var title = $('[name=title]').val();
+      var content = $('[name=content]').val();
+      Posts.insert({
+        name: title,
+        content: content,
+        //wordcount: wordcount,
+        createdAt: new Date()
+      });
+    }
+  })
+
+
 }
